88a27ea12bda097a36704c776bb83f8b24c4cd11
lalnun <lalnun@everest.engineering>
2024-05-15 12:19:12 +0530

    get product items from axios

--- src/customer/components/Product/ProductDisplay.js
+++ src/customer/components/Product/ProductDisplay.js
@@ -1,81 +1,94 @@
 
 
-const products = [
-    {
-        id: 1,
-        name: 'Earthen Bottle',
-        href: '#',
-        price: '$48',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-        imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-    },
-    {
-        id: 2,
-        name: 'Nomad Tumbler',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-        imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-    },
-    {
-        id: 3,
-        name: 'Focus Paper Refill',
-        href: '#',
-        price: '$89',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-    },
-    {
-        id: 4,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    {  
-        id: 5,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    {
-        id: 6,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    {
-        id: 7,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    {
-        id: 8,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    // More products...
-]
+// const products = [
+//     {
+//         id: 1,
+//         name: 'Earthen Bottle',
+//         href: '#',
+//         price: '$48',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+//         imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+//     },
+//     {
+//         id: 2,
+//         name: 'Nomad Tumbler',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+//         imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+//     },
+//     {
+//         id: 3,
+//         name: 'Focus Paper Refill',
+//         href: '#',
+//         price: '$89',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+//         imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+//     },
+//     {
+//         id: 4,
+//         name: 'Machined Mechanical Pencil',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+//         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+//     },
+//     {  
+//         id: 5,
+//         name: 'Machined Mechanical Pencil',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+//         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+//     },
+//     {
+//         id: 6,
+//         name: 'Machined Mechanical Pencil',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+//         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+//     },
+//     {
+//         id: 7,
+//         name: 'Machined Mechanical Pencil',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+//         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+//     },
+//     {
+//         id: 8,
+//         name: 'Machined Mechanical Pencil',
+//         href: '#',
+//         price: '$35',
+//         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+//         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+//     },
+//     // More products...
+// ]
+
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function ProductDisplay() {
+
+    const [products, setProducts] = useState();
+    
+    useEffect(() => {
+        axios.get("url").then((response)=>{
+            setProducts(response.data.products);
+        })
+    }, [])
+
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Products</h2>
 
                 <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
+                    {products?.map((product) => (
                         <a key={product.id} href={product.href} className="group">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                 <img
@@ -92,4 +105,4 @@ export default function ProductDisplay() {
             </div>
         </div>
     )
-}
+} 
